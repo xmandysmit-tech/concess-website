130f8fc97a0330f6903c64ce5f796cd9fd29d05e
Mandy Smit <xmandysmit@gmail.com>
2026-07-26 12:54:59 +0200

    AR filter hero: slightly smaller phone (26vw)

--- app/studio/[slug]/page.tsx
+++ app/studio/[slug]/page.tsx
@@ -74,7 +74,7 @@ export default async function StudioCasePage({ params }: { params: Promise<{ slu
                 <img
                   src={project.heroImage}
                   alt={project.title}
-                  style={{ width: "clamp(240px, 32vw, 480px)", objectFit: "contain", display: "block" }}
+                  style={{ width: "clamp(200px, 26vw, 400px)", objectFit: "contain", display: "block" }}
                 />
               </div>
             </div>
